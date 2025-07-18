923611907298037f58dd52ac45afc0799bd7745f
titanium-dev <akinkunmioye42@gmail.com>
2025-07-18 04:33:13 +0100

    Add PostHog analytics integration and update environment variables in main.tsx; add .env to .gitignore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare interface ImportMetaEnv {
+  readonly VITE_PUBLIC_POSTHOG_KEY: string
+  readonly VITE_PUBLIC_POSTHOG_HOST: string
+  readonly MODE: string
+  // more env variables can be defined here
+}
+
+declare interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
